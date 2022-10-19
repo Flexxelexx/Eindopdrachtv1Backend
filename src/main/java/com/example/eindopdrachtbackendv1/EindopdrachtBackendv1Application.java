4a49a14f89a25d3846d6867c0c0e78ae08e7ed4b
Flexxelexx <alexxkooij@hotmail.com>
2022-10-19 09:30:52 +0200

    first commit

---
+++ src/main/java/com/example/eindopdrachtbackendv1/EindopdrachtBackendv1Application.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtbackendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtBackendv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eindopdrachtbackendv1/EindopdrachtBackendv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtbackendv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
